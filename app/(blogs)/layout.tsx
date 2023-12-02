d856391113ac47c3f3191d72832144c3564be679
pushpa1105 <projectinfo1105@gmail.com>
2023-12-02 16:10:21 +0545

    seo:add

--- app/(blogs)/layout.tsx
+++ app/(blogs)/layout.tsx
@@ -4,12 +4,45 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastProvider } from "@/components/providers/toaster-provider";
 import { ConfettiProvider } from "@/components/providers/confetti-provider";
+import siteMetaData from "@/lib/site-meta-data";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Blogs",
-  description: "Goodwill App - Blogs",
+  metadataBase: new URL(siteMetaData.siteUrl),
+  title: {
+    template: `%s | ${siteMetaData.title}`,
+    default: siteMetaData.title, // a default is required when creating a template
+  },
+  description: siteMetaData.description,
+  openGraph: {
+    title: siteMetaData.title,
+    description: siteMetaData.description,
+    url: siteMetaData.siteUrl,
+    siteName: siteMetaData.title,
+    images: [siteMetaData.socialBanner],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    // nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteMetaData.title,
+    description: siteMetaData.description,
+    images: [siteMetaData.socialBanner],
+  },
 };
 
 export default function RootLayout({
