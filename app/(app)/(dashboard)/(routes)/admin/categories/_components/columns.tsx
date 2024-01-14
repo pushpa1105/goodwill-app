aca9505f5168d2b30dfa199df5fd83ad73339b95
pushpa1105 <projectinfo1105@gmail.com>
2024-01-15 00:07:32 +0545

    categpry-admin

---
+++ app/(app)/(dashboard)/(routes)/admin/categories/_components/columns.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Category, Course } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, Pencil, Trash } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { CategoryModal } from "./category-modal";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+export const columns: ColumnDef<Category>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id, name } = row.original;
+
+      const data = { collection: "Category" };
+      const router = useRouter();
+
+      const onDelete = async () => {
+        try {
+          await axios.delete(`/api/categories/${id}`, {
+            data,
+          });
+          toast.success("Category deleted");
+          router.refresh();
+        } catch (error) {
+          if (error?.response?.data) toast.error(error?.response?.data);
+          else toast.error("Something went wrong");
+        }
+      };
+
+      return (
+        <>
+          <div className="flex flex-wrap">
+            <div>
+              <CategoryModal collection="Category" name={name} id={id}>
+                <Pencil className="h-4 w-4 mr-2 hover:text-indigo-600 cursor-pointer" />
+              </CategoryModal>
+            </div>
+            <div>
+              <ConfirmModal onConfirm={onDelete}>
+                <Trash className="h-4 w-4 mr-2 hover:text-indigo-600 cursor-pointer" />
+              </ConfirmModal>
+            </div>
+          </div>
+        </>
+      );
+    },
+    header: "Actions",
+  },
+];
---
+++ app/(app)/api/categories/route.ts
@@ -0,0 +1,33 @@
+import { isAdmin } from "@/lib/admin";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const { values, collection } = await req.json();
+
+    const isAuthorized = isAdmin(userId);
+
+    if (!userId || !isAuthorized) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    let category;
+    if (collection === "Category") {
+      category = await db.category.create({
+        data: values,
+      });
+    } else {
+      category = await db.blogCategory.create({
+        data: values,
+      });
+    }
+
+    return NextResponse.json(category);
+  } catch (error) {
+    console.log("[CATEGORY]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
