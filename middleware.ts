b72c8ba7eaf7d7f451ea63f4be2a762e3677ef03
pushpa1105 <projectinfo1105@gmail.com>
2024-06-16 17:47:13 +0545

    test: auth

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,33 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: [
-    "/api/webhook",
-    "/api/webhooks/user",
-    "/",
-    "/api/uploadthing",
-    "/api/webinars/:slug/subscribe",
-    "/api/blogs/:id/views",
-    "/blogs",
-    "/blogs/:slug",
-    "/products",
-    "/webinars",
-    "/webinars/:slug",
-    "/account-opening",
-    "/courses",
-    "/courses/:slug",
-    "/about-us",
-    "/privacy-policy",
-    "/terms",
-  ],
+const publicRoutes = [
+  "/api/webhook",
+  "/api/webhooks/user",
+  "/",
+  "/api/uploadthing",
+  "/api/webinars/:slug/subscribe",
+  "/api/blogs/:id/views",
+  "/blogs",
+  "/blogs/:slug",
+  "/products",
+  "/webinars",
+  "/webinars/:slug",
+  "/account-opening",
+  "/courses",
+  "/courses/:slug",
+  "/about-us",
+  "/privacy-policy",
+  "/terms",
+];
+
+export default withClerkMiddleware((req: NextRequest) => {
+  console.log(req.nextUrl.pathname);
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
+
